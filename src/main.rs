eb3f83a930853124a90cc48577c84a5900e04634
DrJupiter <klausjupiter@hotmail.com>
2021-03-28 18:14:22 +0200

    Design doc

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 mod libpacman;
+use core::str::next_code_point;
+
+use libpacman::{count_ghosts, read_value};
 
 fn main() {
-    println!("Hello, world!");
+    let n = read_value::<u32>().unwrap();
+    println!("{}", count_ghosts(n));
 }
+
+
